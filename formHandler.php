b6287eb6af7cb27c87471c5a904ac911e177cfe0
Thomas Hewett <up665064@myport.ac.uk>
2015-01-10 23:42:37 +0000

    Index, Drawer and FormHandler (basic)
    Index.php landing page loads the 'drawer' through a PHP 'include' (thus,
    why it doesn't look like a drawer). Drawer.html has a basic version of
    the form the user will fill out. Few options omitted such as 'latest
    start date' as these will be calculated automatically in the future.
    formHandler.php is what the 'submit' button on the form triggers, this
    currently just echo's the information entered by user.

---
+++ formHandler.php
@@ -0,0 +1,6 @@
+<?php
+$taskName = $_POST['tName'];
+$earliestStart = $_POST['eStart'];
+$earliestFinish = $_POST['eFinish'];
+echo "Task Name: ".$taskName.PHP_EOL."Earliest start: ".$earliestStart.PHP_EOL."Earliest Finish: ".$earliestFinish;
+?>
