5f8fd767909e4950b923a4eba8d570d659a640f0
Thomas Hewett <up665064@myport.ac.uk>
2015-01-20 22:00:44 +0000

    Drawer & Example Node
    Using Javascipt(wbt.js) and CSS(base.css), the Drawer.html is now pulled
    into the screen when a user clicks on the node, and is pushed out when
    the user clicks outside the drawer.
    
    Using the Raphael.Js library, I have created an example node.

--- index.php
+++ index.php
@@ -2,12 +2,17 @@
 
 <head>
     <title>Schedule Me - Week 1</title>
-    <link rel=stylesheet href="base.css"> 
+    <link rel=stylesheet href="base.css">
+    <script src="raphael-min.js"></script>
+    <script src="wbt.js"></script>
 </head>
 
+
 <body>
-    <h1>Schedule Me - Week 1</h1>
-    <h2>Database Link Experiment</h2>
-    
-    <?php include "drawer.html"; ?>
+    <?php include 'drawer.html' ?>
+    <div id="svgCanvas">
+        <h1>Schedule Me - Week 1</h1>
+        <h2>Database Link Experiment</h2>
+        <div id="jCanvas" class="cCanvas"></div>
+    </div>
 </body>
