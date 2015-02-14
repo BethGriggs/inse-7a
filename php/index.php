06e4814b50aaef0db740020b45fbde54605620d1
Thomas Hewett <up665064@myport.ac.uk>
2015-02-14 15:57:22 +0000

    input-date-validation
    #closes 3
    #closes 4
    
    Major Changes:
    + Validates earliest start and earliest finish dates and gives feedback
    to the user
    + Won't allow form submission if dates not valid
    
    Minor Changes:
    + Form does not submit on "enter" keypress (was defunct anyway as it
    would redirect to an old php script)

--- php/index.php
+++ php/index.php
@@ -1,11 +1,12 @@
 <?php
 
 
-echo '<form id="add_task" action ="dbinsert.php" method="POST"'.
+echo '<form id="add_task" onsubmit="return false;">'.
      '<label for="tName">Task Name</label>'.
                 '<input type="text" id="tName" name="tName" >'.
                 '<label for="eStart">Earliest Start</label>'.
                 '<input type="date" id="eStart" class="datepicker" name="eStart" >'.
+                '<p id="isDateValid"></p>'.
                 '<label for="eFinish">Earliest Finish</label>'.
                 '<input type="date" id="eFinish" class="datepicker" name="eFinish" >'.
                 '<input type="button" onclick="PostData()" value="Add Task">'.
--- script/wbt.js
+++ script/wbt.js
@@ -1,39 +1,82 @@
+/** SVG CANVAS - CONTENT WINDOW **********************************************/
+
 var node;
 
 function drawCanvas() {
     var paper = Raphael(document.getElementById("jCanvas"), 300, 300);
     // Creates the SVG Canvas
-    var rect = paper.rect(100, 150, 100, 50, 10).attr({"fill":"#525252", "stroke-width":"0"});
-    var text = paper.text(150, 175, "This is my text").attr({"fill":"#bbbbbb", "font-size":"15em"});
+    var rect = paper.rect(100, 150, 100, 50, 10).attr(
+        {"fill":"#525252", "stroke-width":"0"});
+    var text = paper.text(150, 175, "This is my text").attr(
+        {"fill":"#bbbbbb", "font-size":"15em"});
     // Creates the Rounded Rectangle and Text
     node = paper.set();
     node.push(text);
     node.push(rect);
     // Creates a new Set of elements and pushes the shapes into it
     node.hover(nodeHoverIn, nodeHoverOut, rect, rect);
-    // element.hover(hoverIn function, hoverOut function, hoverIn object(to manipulate), hoverOut object)
+    // element.hover(hoverIn function, hoverOut function, hoverIn 
+    // object(to manipulate), hoverOut object)
     node.click(openForm); 
     // Raphael event listener
-    node.addClass('taskNode');
+    //node.addClass('taskNode');
     // Potentially able to manipulate in CSS as it has a class?
 }
 
 function nodeHoverIn(){
     // Enlarges the node under mouse hover
-    this.animate({"x":"90", "y":"140", "width": "120", "height":"70"}, 1000, "bounce");
+    this.animate(
+        {"x":"90", "y":"140", "width": "120", "height":"70"}, 1000, "bounce");
 }
 
 function nodeHoverOut(){
     // Node goes back to original size when not under hover
-    this.animate({"x":"100", "y":"150", "width": "100", "height":"50"}, 1000, "bounce");
+    this.animate(
+        {"x":"100", "y":"150", "width": "100", "height":"50"}, 1000, "bounce");
 }
 
 function openForm(event){
     div_show();
-    event.stopImmediatePropagation(); //Stops the next listener from being fired automatically
+    dateListeners();
+    event.stopImmediatePropagation(); 
+    //Stops the next listener from being fired automatically
     document.getElementById('svgCanvas').addEventListener('click', div_hide, false);
 }
 
+/** FORM VALIDATION **********************************************************/
+
+function dateListeners() {
+    var eStart = document.getElementById('eStart');
+    var eFinish = document.getElementById('eFinish');
+    
+    eStart.addEventListener('blur', dateCheckText, false);
+    eStart.addEventListener('change', dateCheckText, false);
+    eFinish.addEventListener('blur', dateCheckText, false);
+    eFinish.addEventListener('change', dateCheckText, false); 
+}
+
+function isDateValid() {
+    var eStartDate = new Date(document.getElementById('eStart').value);
+    var eFinishDate = new Date(document.getElementById('eFinish').value);
+    if(eStartDate <= eFinishDate){ return true; } 
+    return false;
+}
+
+function dateCheckText() {
+    var dateCheck = document.getElementById('isDateValid');
+    var validDate = isDateValid();
+    
+    if(validDate){
+        dateCheck.innerHTML = "Dates are valid";
+        dateCheck.style.color = "#00FF00";
+    } else if(!validDate){
+        dateCheck.innerHTML = "Dates are invalid, please ensure your " +
+            "start date is before or the same as the finish date";
+        dateCheck.style.color = "#FF0000";
+    }
+}
+
+/** SECTION HIDING ***********************************************************/
 
 function div_hide() {
     document.getElementById('navigation').style.left = '-20%';
