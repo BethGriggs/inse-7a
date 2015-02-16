c23cf3a6698e00c0c780255a75daaea3b5d6d887
BethGriggs <bethanygriggs@outlook.com>
2015-02-16 11:52:52 +0000

    Change to SQL to ensure task_name is UNIQUE
    You must drop and recreate the table in phpmyadmin for this to come into
    effect. Further work to be done in creating an error message

--- php/index.php
+++ php/index.php
@@ -24,7 +24,7 @@ try{
 
   $tasktable = "CREATE TABLE IF NOT EXISTS task(
   	task_id INT(11) NOT NULL AUTO_INCREMENT,
-  	task_name VARCHAR(40) NOT NULL,
+  	task_name VARCHAR(40) NOT NULL UNIQUE,
   	task_estart DATE NOT NULL,
   	task_efinish DATE NOT NULL,
   	PRIMARY KEY(task_id));";
